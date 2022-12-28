ae73374a15821a7a1b91617b9dde8168bcab8d61
asmith175 <asmith175@uiowa.edu>
2022-12-28 08:30:34 -0600

    first

---
+++ Scripts/Global.gd
@@ -0,0 +1,45 @@
+extends Node
+
+var max_lives = 4
+var lives = max_lives
+var coins = 0
+var position_lost = false
+var item1_price = "10"
+var item2_price = "10"
+var save_num = 1
+var last_save_x = 0
+var last_save_y = 0
+
+var jump_positions = []
+
+var canTalk = false
+var talkOver = false
+
+var scn
+var hud
+var shop
+var player
+
+func lose_life():
+	lives -= 1
+	hud.load_hearts()
+	if lives <= 0:
+		position_lost = true
+		player.save_position(last_save_x,last_save_y)
+		lives = max_lives
+		hud.load_hearts()
+	
+func collect_money(coinType):
+	if (coinType == 0):
+		coins += 1
+	if (coinType == 1):
+		coins += 5
+	if (coinType == 2):
+		coins += 10
+	hud.load_coins()
+	
+func set_save_position(x,y):
+	last_save_x = x
+	last_save_y = y
+
+	
---
+++ Scripts/Player.gd
@@ -0,0 +1,110 @@
+extends KinematicBody2D
+
+var MOVE_SPEED = 100
+const JUMP_FORCE = 225
+var GRAVITY = 15
+const MAX_FALL_SPEED_UP = 300
+const MAX_FALL_SPEED_DOWN = -300
+
+onready var anim_player = $AnimationPlayer
+onready var sprite = $Sprite
+onready var ammo_sprite = $Ammo
+onready var timer = $Timer
+
+var velocity = Vector2(0,0)
+var y_velo = 0
+
+var facing_right = false
+var facing_up = false
+var lift = true
+
+func _ready():
+	Global.player = self
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
+	
+	if Input.is_action_pressed("fullscreen"):
+		OS.window_fullscreen = !OS.window_fullscreen
+
+	var move_dir = 0
+	var grounded = is_on_floor() || is_on_ceiling()
+	if Input.is_action_pressed("right"):
+		move_dir += 1
+		velocity.x = 1
+	elif Input.is_action_pressed("left"):
+		move_dir -= 1
+		velocity.x = -1
+	
+	if Input.is_action_just_released("flip_gravity_up"):
+		if grounded:
+			y_velo = -10
+			play_anim("blob")
+			lift = false
+			grounded = false
+		GRAVITY = -15
+	
+	if Input.is_action_just_released("flip_gravity_down"):
+		if grounded:
+			y_velo = 10
+			play_anim("blob")
+			lift = false
+			grounded = false
+		GRAVITY = 15
+	
+	y_velo += GRAVITY
+	
+	if y_velo > MAX_FALL_SPEED_UP:
+		y_velo = MAX_FALL_SPEED_UP
+	if y_velo < MAX_FALL_SPEED_DOWN:
+		y_velo = MAX_FALL_SPEED_DOWN
+	
+	move_and_slide(Vector2(velocity.x * MOVE_SPEED, y_velo), Vector2(0, -1))
+	
+	velocity.x = lerp(velocity.x,0,0.5)
+		
+	if facing_right and move_dir > 0:
+		flip_h()
+	if !facing_right and move_dir < 0:
+		flip_h()
+	if facing_up and y_velo > 0 and lift:
+		flip_v()
+	if !facing_up and y_velo < 0 and lift:
+		flip_v()
+		
+	if grounded:
+		if move_dir == 0:
+			if facing_up:
+				play_anim("idle_flip")
+			else:
+				play_anim("idle")
+		else:
+			if facing_up:
+				play_anim("move_flip")
+			else:
+				play_anim("move")
+	else:
+		if y_velo < 0 and lift:
+			play_anim("float")
+		elif y_velo > 0 and lift:
+			play_anim("float")
+		
+func flip_h():
+	facing_right = !facing_right
+	sprite.flip_h = !sprite.flip_h
+
+func flip_v():
+	facing_up = !facing_up
+	sprite.flip_v = !sprite.flip_v
+	ammo_sprite.flip_v = !ammo_sprite.flip_v
+
+func play_anim(anim_name):
+	if anim_player.is_playing() and anim_player.current_animation == anim_name:
+		return
+	anim_player.play(anim_name)
+
+
+func _on_AnimationPlayer_animation_finished(anim_name = "blob"):
+	lift = true
